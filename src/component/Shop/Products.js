7e6aa6f5a7769b0f097e1d98f040e739ea68e3df
shubhampatil.ani@gmail.com <shubhampatil.ani@gmail.com>
2023-03-18 11:27:36 +0530

    added

--- src/component/Shop/Products.js
+++ src/component/Shop/Products.js
@@ -1,16 +1,41 @@
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "A Product",
+    price: 8,
+    description: "anything we can write here for first product",
+  },
+  {
+    id: "p2",
+    title: "A Second Product",
+    price: 8,
+    description: "anything we can write here for second product",
+  },
+  {
+    id: "p3",
+    title: "A  Third Product",
+    price: 10,
+    description: "anything we can write here for third product",
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
